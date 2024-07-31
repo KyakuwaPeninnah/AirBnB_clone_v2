de7dc036597afefc55243f4933e5570ee088495c
root <you@example.com>
2024-07-31 01:57:31 -0700

    improve engine

--- models/state.py
+++ models/state.py
@@ -5,18 +5,20 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 from models import storage_type
+from os import getenv
+
+STORAGE_TYPE = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    if storage_type == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
-    else:
+        if STORAGE_TYPE != "db"
         name = ""
-
+        cities = []
         @property
         def cities(self):
             """returns list of city instances"""
